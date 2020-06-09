eb62c07f11e0d5d3205240042ed4364a4d827c1c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-06-09 12:22:20 +0200

    changed restaurant-list

--- src/components/restaurantCard.js
+++ src/components/restaurantCard.js
@@ -3,21 +3,49 @@ import { NavLink } from "react-router-dom"
 
 const RestaurantCard = (props) => {
 	console.log(props.entireRestaurant.id)
+
+	let stars = []
+	for (let i = 0; i < props.rating; i++) {
+		stars.push(<i className='material-icons'>star_rate</i>)
+	}
+
 	return (
 		<div className='single-restaurant-card'>
-			<h2>{props.name}</h2>
-			<span>{props.tags}</span>
-			<ul>
-				<li>{props.options}</li>
-			</ul>
-			<NavLink
-				to={{
-					pathname: `/restaurant/${props.entireRestaurant.id}`,
-                    state: props.entireRestaurant
-				}}
-			>
-				<span>Zie menu.</span>
-			</NavLink>
+			<div className='restaurant-card-content-wrapper'>
+				<div className='restaurant-card-header'>
+					<div className='restaurant-card-title'>
+						<h2>{props.name}</h2>
+						<span className='restaurant-card-tags'>{props.tags}</span>
+					</div>
+					<div className='restaurant-card-rating'>
+						{" "}
+						{stars.map((star) => star)}
+					</div>
+				</div>
+
+				<ul>
+					{props.options.map((option) => {
+						return (
+							<li>
+								<span class='material-icons'>check</span>
+								{option}
+							</li>
+						)
+					})}
+				</ul>
+
+				<NavLink
+					to={{
+						pathname: `/restaurant/${props.entireRestaurant.id}`,
+						state: props.entireRestaurant
+					}}
+				>
+					<div className='restaurant-card-footer'>
+						<span>Zie menu.</span>
+						<i className='material-icons'>navigate_next</i>
+					</div>
+				</NavLink>
+			</div>
 		</div>
 	)
 }
--- src/pages/restaurants.js
+++ src/pages/restaurants.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react"
-import RestaurantCard from '../components/restaurantCard'
+import RestaurantCard from "../components/restaurantCard"
 
 class Restaurants extends Component {
 	state = {
-		postcode: "3021sj",
+		postcode: "3021PX",
 		restaurants: [
 			{
 				id: 1,
 				name: "Beren",
-				tags: ["All You Can Eat", "Barbecue", "Kip"],
+				tags: ["All You Can Eat, ", "Barbecue, ", "Kip "],
 				accessibility: [
 					"Opstapjes",
 					"Goede inrichting",
@@ -19,74 +19,99 @@ class Restaurants extends Component {
 				rating: 4,
 				categories: [
 					[
-                        {
-						name: "Voorgerecht",
-						items: [
-							{
-								name: "Spareribs",
-								price: 8.0,
-								ingredients: ["Varkensvlees", "BBQ Saus"]
-							},
-							{
-								name: "Pizza",
-								price: 8.0,
-								ingredients: ["Varkensvlees", "BBQ Saus"]
-							},
-							{
-								name: "Hamburger",
-								price: 8.0,
-								ingredients: ["Varkensvlees", "BBQ Saus"]
-							},
-							{
-								name: "Carpaccio",
-								price: 8.0,
-								ingredients: ["Varkensvlees", "BBQ Saus"]
-							}
-                        ]
-                    }
-                    ],
-					[ {
-						name: "Hoofdgerecht",
-						items: [{}]
-                    }
-                    ],
-					[   { 
-						name: "Desert",
-                        items: [{}]
-                    }
-                    ],
+						{
+							name: "Voorgerecht",
+							items: [
+								{
+									name: "Spareribs",
+									price: 8.0,
+									ingredients: ["Varkensvlees", "BBQ Saus"]
+								},
+								{
+									name: "Pizza",
+									price: 8.0,
+									ingredients: ["Varkensvlees", "BBQ Saus"]
+								},
+								{
+									name: "Hamburger",
+									price: 8.0,
+									ingredients: ["Varkensvlees", "BBQ Saus"]
+								},
+								{
+									name: "Carpaccio",
+									price: 8.0,
+									ingredients: ["Varkensvlees", "BBQ Saus"]
+								}
+							]
+						}
+					],
 					[
-                        { 
-						name: "Drinken",
-                        items: [{}]
-                        }
-                    ]
+						{
+							name: "Hoofdgerecht",
+							items: [{}]
+						}
+					],
+					[
+						{
+							name: "Desert",
+							items: [{}]
+						}
+					],
+					[
+						{
+							name: "Drinken",
+							items: [{}]
+						}
+					]
 				]
 			}
 		]
 	}
 
 	render() {
-        let singleRestaurantcard = null; 
-        
-        this.state.restaurants.map((restaurant) => (
-            singleRestaurantcard = 
-            <RestaurantCard 
-                 key={restaurant.id}
-                 entireRestaurant={restaurant}
-                 name={restaurant.name}
-                 options={restaurant.accessibility}
-                 rating={restaurant.rating}
-                 tags={restaurant.tags}
-            />
-        ))
+		let singleRestaurantcard = null
+
+		this.state.restaurants.map(
+			(restaurant) =>
+				(singleRestaurantcard = (
+					<RestaurantCard
+						key={restaurant.id}
+						entireRestaurant={restaurant}
+						name={restaurant.name}
+						options={restaurant.accessibility}
+						rating={restaurant.rating}
+						tags={restaurant.tags}
+					/>
+				))
+		)
 
 		return (
 			<div>
-				<h1>Restaurants</h1>
-                <div className="restaurantsWrapper">
-                {singleRestaurantcard}
-                </div>
+				<div className='restaurants-list-title'>
+					<h1>Restaurants</h1>
+					<span>{this.state.postcode}</span>
+				</div>
+
+				<ul className='restaurants-list-filter'>
+					<li>
+						<strong>Alles</strong>
+					</li>
+					<li>Turks</li>
+					<li>Italiaans</li>
+					<li>Surinaams</li>
+					<li>Frans</li>
+					<li>Indiaas</li>
+					<li>Comfort</li>
+					<li>Spareribs</li>
+					<li>Patat</li>
+					<li>
+						<strong>Alle categorieen</strong>
+					</li>
+				</ul>
+
+				<hr class='solid' />
+
+				<div className='restaurantsWrapper'>{singleRestaurantcard}</div>
 			</div>
 		)
 	}
